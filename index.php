1c4d8ddad462946744635713a3100e893bc7d7ff
Manish <manishprajapatim416@gmail.com>
2021-12-30 20:52:48 +0530

    Slider done

--- index.php
+++ index.php
@@ -18,5 +18,36 @@
       <a href="#" id="loginBtn">Login/Register</a>
     </div>
   </div>
+  <div class="slider">
+    <input type="radio" name="slider" checked="checked">
+    <div class="imgBox">
+      <img src="./images/vaccination1.jpg">
+      <div class="content">
+        <h2>Vaccination saves lives</h2>
+        <p>Most COVID-19 vaccines require two doses.</p>
+        <p>Getting your both doses, as soon as it's available to you, will give you the best protection against COVID-19.</p>
+        <a href="#">Book Your Vaccine</a>
+      </div>
+    </div>
+    <input type="radio" name="slider">
+    <div class="imgBox">
+      <img src="./images/leb1.jpg">
+      <div class="content">
+        <h2>Slide Two</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero iusto illo cumque cum repudiandae! Facere quis recusandae aliquam labore eum.</p>
+        <a href="#">Read more</a>
+      </div>
+    </div>
+    <input type="radio" name="slider">
+    <div class="imgBox">
+      <img src="./images/beds1.jpg">
+      <div class="content">
+        <h2>Slide Three</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero iusto illo cumque cum repudiandae! Facere quis recusandae aliquam labore eum.</p>
+        <a href="#">Read more</a>
+      </div>
+    </div>
+
+  </div>
 </body>
 </html>
