a0ed5a9d5b53a21e2f7c7d3173f94dc159bde61a
Manish <manishprajapatim416@gmail.com>
2022-03-26 21:54:16 +0530

    Hospital Accept Bed Request Done (FullPageRemains)

---
+++ hospital/bedRequests.php
@@ -0,0 +1,98 @@
+<?php include('./includes/header.php'); ?>
+<?php include('./includes/navigation.php'); ?>
+<?php include('./includes/db.php'); ?>
+
+<?php
+    if(isset($_GET['accept'])){
+        $patient_id = $_GET['accept'];
+        $query = "UPDATE bed_requests SET patient_status='accepted' WHERE patient_id = $patient_id";
+        $accepted = mysqli_query($connection,$query);
+        if(!$accepted){
+            die("QUERY FAILED " . mysqli_error($connection));
+        }else{
+            echo '<script>
+            alert("Request Accepted");
+            window.location.href="bedRequests.php";
+            </script>';
+        }
+    }
+
+    if(isset($_GET['reject'])){
+        $patient_id = $_GET['reject'];
+        $query = "UPDATE bed_requests SET patient_status='rejected' WHERE patient_id = $patient_id";
+        $accepted = mysqli_query($connection,$query);
+        if(!$accepted){
+            die("QUERY FAILED " . mysqli_error($connection));
+        }else{
+            echo '<script>
+            alert("Request Rejected");
+            window.location.href="bedRequests.php";
+            </script>';
+        }
+    }
+?>
+
+<div class="container pt-4">
+    <div class="d-flex justify-content-between mb-2">
+        <h5>Lab Testing Requests:</h5>
+    </div>
+
+    <!-- Testing table -->
+    <div class="card px-5 py-3 shadow">
+        <table class="table">
+            <thead>
+                <tr>
+                <th scope="col">#</th>
+                <th scope="col">Date</th>
+                <th scope="col">User Name</th>
+                <th scope="col">Ward Name</th>
+                <th scope="col" colspan="2">Commands</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                    $query = "SELECT * FROM bed_requests INNER JOIN users ON users.user_id = bed_requests.user_id WHERE hospital_id = $global_hospital_id";
+                    $getBedRequests = mysqli_query($connection,$query);
+                    if(mysqli_num_rows($getBedRequests)==0){
+                        echo "<tr><td colspan='4' class='text-center'>No Requests Found.</td></tr>";
+                    }else{
+                        $index = 1;
+                        while($row=mysqli_fetch_array($getBedRequests)){
+                            $user_name = $row['user_name'];
+                            $patient_id  = $row['patient_id'];
+                            $date = $row['request_date'];
+                            $patient_status = $row['patient_status'];
+                            $query = "SELECT * FROM bed_requests INNER JOIN ward_details ON bed_requests.ward_id = ward_details.ward_id WHERE patient_id = '{$patient_id}'";
+                            $getWard = mysqli_query($connection,$query);
+                            while($row = mysqli_fetch_array($getWard)){
+                                $ward_name = $row['ward_name'];
+                            }
+                            if($patient_status=='accepted'){
+                                echo "<tr class='table-success'>";
+                            }else if($patient_status=='rejected'){
+                                echo "<tr class='table-danger'>";
+                            }else{
+                                echo "<tr>";
+                            }
+                            echo "
+                                <th scope='row'>$index</th>
+                                <td>$date</td>
+                                <th scope='row'><a href='viewRequest.php?patient_id=$patient_id' class='link-dark' style='text-decoration:none;'>$user_name</a></th>
+                                <td>$ward_name</td>";
+                                if($patient_status=="pending"){
+                                    echo "<td><a href='bedRequests.php?accept=$patient_id' class='card-link text-secondary' onClick=\"javascript: return confirm('Are You Sure?');\">Accept</a></td>
+                                    <td><a href='bedRequests.php?reject=$patient_id' class='card-link text-secondary' onClick=\"javascript: return confirm('Are You Sure?');\">Reject</a></td>
+                                    </tr>
+                                    ";
+                                }else{
+                                    echo "<td>--</td>";
+                                    echo "<td>--</td>";
+                                }
+                            $index++;
+                        }
+                    }
+                ?>
+            </tbody>
+        </table>
+    </div>
+</div>
--- hospital/includes/navigation.php
+++ hospital/includes/navigation.php
@@ -2,11 +2,18 @@
   <div class="container">
     <a class="navbar-brand mb-0 h1 fs-4" href="./"><?php echo $global_hospital_name;?></a>
 
-    <ul class="navbar-nav">
-      <li class="nav-item">
-        <a class="nav-link fs-5" href="./wardDetails.php">Ward Details</a>
+    <div class="d-flex">
+      <ul class="navbar-nav mx-3">
+        <li class="nav-item">
+          <a class="nav-link fs-5" href="./wardDetails.php">Ward Details</a>
+        </li>
+      </ul>
+      <ul class="navbar-nav mx-3">
+        <li class="nav-item">
+          <a class="nav-link fs-5" href="./bedRequests.php">Bed Requests</a>
+        </ul>
       </li>
-    </ul>
+    </div>
 
     <form class="d-flex">
       <a href="./includes/logout.php" class="btn btn-outline-danger mb-0 fs-5 rounded-pill px-4" type="submit">Logout</a>
